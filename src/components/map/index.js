c08b541c2ec6e585040c492ffc0df043f26554bf
Joshua Ho <jho@innocean.ca>
2017-07-21 14:55:05 -0400

    updates

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -7,7 +7,9 @@ class GoogleMap extends Component {
 
     this.state = {
       map: null,
-      markers : []
+      markers : [],
+      lat : 0,
+      lng : 0
     };
   }
 
@@ -20,19 +22,31 @@ class GoogleMap extends Component {
       });
 
       this.setState({
-        map : mapObj
+        map : mapObj,
+        lat : this.props.lat,
+        lng : this.props.lng
       })
     }
   }
 
-  addMarker( location, map ) {
+  addMarker( location, map, data ) {
     const gmap = this.props.gmap;
     let marker = new gmap.Marker({
       position: location,
       map: map
     })
 
+    if( data ) {
+      marker.data = data;
+      marker.addListener( 'click', this.markerClickHandler.bind(this, marker) )
+    }
+
     this.state.markers.push( marker )
+  }
+
+  markerClickHandler( marker ) {
+    console.log( this, marker, arguments )
+    this.props.markerCallback( marker.data )
   }
 
   componentWillUpdate( nextProps ) {
@@ -49,10 +63,15 @@ class GoogleMap extends Component {
         if( status === 'OK' ){
           var i;
           for( i = 0; i < results.length; i++ ){
-            this.addMarker( new gmap.LatLng( results[i].geometry.location.lat(), results[i].geometry.location.lng() ), this.state.map );
+            this.addMarker( new gmap.LatLng( results[i].geometry.location.lat(), results[i].geometry.location.lng() ), this.state.map, nextProps.data );
           }
         }
       });
+    }
+
+    if( this.state.map && ( this.state.lat !== nextProps.lat || this.state.lng !== nextProps.lng ) ) {
+      const gmap = this.props.gmap;
+      this.state.map.setCenter( new gmap.LatLng( nextProps.lat, nextProps.lng ), this.state.map )
     }
   }
 
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import GoogleMap from '../components/map/'
+import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import { addItem } from '../actions/'
 
 class Landing extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      lat : 43.6525,
+      lng : -79.381667,
+      markerData : null
+    }
+
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition( this.successHandler.bind(this), this.errorHandler, options );
+  }
+
+  successHandler( pos ) {
+    this.setState({
+      lat : pos.coords.latitude,
+      lng : pos.coords.longitude
+    })
+    console.log( pos.coords )
+  }
+
+  errorHandler( err ) {
+    console.log( "error loading geolocation", err );
+  }
+
+  markerselect( data ) {
+    this.setState({
+      markerData : data
+    })
+  }
+
+  clickHandler() {
+    this.setState({
+      markerData : null
+    })
+  }
+
   render(){
     let dataType = null;
     let style = {
       height: '100%'
     }
 
+    let cardStyle = {
+      height: '50%',
+      display: 'none'
+    }
+
+    let cardTitle = "";
+    let cardData = "";
+
     if( this.props.data ) {
       dataType = this.props.data.todo;
+    }
+
+    if( this.state.markerData ) {
       style = {
         height: '50%'
       }
+      cardStyle.display = 'block'
+      cardTitle = this.state.markerData.todo;
+      cardData = this.state.markerData.todoAction
     }
-    
+
     return(
-      <section style={style}>
-        <GoogleMap container="mapContainer" lat={43.6525} lng={-79.381667} showDefaultCursor={true} type={dataType} />
+      <section className="mid-content">
+        <section className="map-container" style={style}>
+          <GoogleMap container="mapContainer" lat={this.state.lat} lng={this.state.lng} showDefaultCursor={true} type={dataType} data={this.props.data} markerCallback={this.markerselect.bind(this)} />
+        </section>
+        <section style={cardStyle}>
+          <Card>
+            <CardTitle
+              title={cardTitle}
+            />
+          <CardText>
+            {cardData}
+          </CardText>
+          <CardActions>
+            <FlatButton label="Got it" onClick={this.clickHandler.bind(this)}/>
+          </CardActions>
+          </Card>
+        </section>
       </section>
     )
   }
